9b872c8e0cb5f51e66d072a02c4bde516634ec9e
seongminQa <129333587+seongminQa@users.noreply.github.com>
2024-03-11 00:20:09 +0900

    객체를 활용하여 간단한 계좌 시스템 문제
    첫번째, Account 클래스에서 간단한 필드 3개와 기본 생성자로만 사용
    두번째, Account 클래스내에서 메소드를 선언하여 메소드 활용하려 시도

---
+++ 6장 20번문제/first/Account.java
@@ -0,0 +1,12 @@
+package subject.marchnine.twenty.first;
+
+public class Account {
+
+	// 필드 선언
+	public String account; // 계좌번호
+	public String name; // 계좌주
+	public int balance; // 잔액
+
+	// 생성자 선언
+	Account() {}
+}
